f21f9b2419ddcf63d4987ab20f6f462e1d29c75a
smattoo2 <matooshikhar@gmail.com>
2026-06-08 14:36:02 -0700

    added CORs origin

--- backend/server.py
+++ backend/server.py
@@ -116,7 +116,7 @@ app = FastAPI(title="GLP Onboarding API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://dlalry1vlg7a1.cloudfront.net"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
